00e31ab04da9756316f6f45560b055ddbdfa81f2
Mayong Saputra <gilang46622@gmail.com>
2025-09-08 11:46:05 +0700

    Finishing & fixing: Add Animations and fix bugs

--- javascript/header.js
+++
@@ -1,6 +0,0 @@
-const listBar = document.querySelector(".list-bar");
-const navSlide = document.querySelector("nav ul");
-
-listBar.addEventListener("click", () => {
-  navSlide.classList.toggle("slide");
-});
